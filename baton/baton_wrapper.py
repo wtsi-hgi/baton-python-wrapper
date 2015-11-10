b0b5a53611cf8866aeccb3759880c2fe7164216f
Colin Nolan <colin.nolan@sanger.ac.uk>
2015-11-10 14:26:21 +0000

    Further refactoring.

--- baton/baton_wrapper.py
+++ baton/baton_wrapper.py
@@ -18,7 +18,7 @@ class Baton:
         self._baton_location = baton_location
         self._irods_query_zone = irods_query_zone
 
-    def query_by_metadata_and_get_results_as_json(self, avu_tuple_list, operator='='):
+    def query_by_metadata_and_get_results_as_json(self, avu_tuple_list, operator="="):
         """
         This method is querying iRODS using BATON in order to get the metadata for the files (data objects) that match the search criteria.
         The information is returned as a dict of collection, data_object and avus. It can be filtered afterwards for leaving in only the info of interest.
@@ -28,7 +28,7 @@ class Baton:
         :return: a tempfile
         WARNING:
             1. This assumes that the operator is always =
-            2. This assumes that there is exactly 1 entry for each type of attribute - there can't be a query for 2 samples for exp.
+            2. This assumes that there is exactly 1 entry for each type of attribute - there can"t be a query for 2 samples for exp.
         """
         irods_avus = self._convert_to_baton_avus(avu_tuple_list)
         irods_avus_json = json.dumps(irods_avus)
@@ -49,7 +49,6 @@ class Baton:
         :param file_paths:
         :return:
         """
-        # TODO: This probably has commonality with `get_file_metadata` yet it doesn't use it
         list_of_fpaths_as_json = []
         for file_path in file_paths:
             irods_fpath_dict_as_json = self.get_file_metadata(file_path)
@@ -62,8 +61,9 @@ class Baton:
         :param query_as_json:
         :return: the path to a temp file where the results are
         """
-        # Note: it is not necessary to add also '--checksum' if --replicate is there
-        return self.run_query([self._baton_location, '--zone', self._irods_query_zone, '--obj', '--checksum', '--avu', '--acl'])
+        # Note: it is not necessary to add also "--checksum" if --replicate is there
+        return self.run_query(
+            [self._baton_location, "--zone", self._irods_query_zone, "--obj", "--checksum", "--avu", "--acl"])
 
     def _get_baton_list_metadata_result(self, data_obj_as_json):
         """
@@ -71,9 +71,9 @@ class Baton:
         :param data_obj_as_json:
         :return:
         """
-        #jq -n '[{data_object: "10080_8#64.bam", collection: "/seq/10080/"}]' | /software/gapi/pkg/baton/0.15.0/bin/baton-list -avu --acl
-        # Note: it is not necessary to add also '--checksum' if --replicate is there
-        return Baton.run_query([self._baton_location, '--avu', '--acl', '--checksum'], data_obj_as_json)
+        # Note: it is not necessary to add also "--checksum" if --replicate is there
+        return Baton.run_query([self._baton_location, "--avu", "--acl", "--checksum"], input_data=data_obj_as_json)
+        #jq -n "[{data_object: "10080_8#64.bam", collection: "/seq/10080/"}]" | /software/gapi/pkg/baton/0.15.0/bin/baton-list -avu --acl
 
     def _get_baton_list_metadata_for_list_of_files_result(self, list_of_data_obj_as_json):
         """
@@ -82,14 +82,15 @@ class Baton:
         :return:
         """
         return Baton.run_query(
-            [self._baton_location, '--avu', '--acl', '--checksum'], write_to_standard_in=list_of_data_obj_as_json)
+            [self._baton_location, "--avu", "--acl", "--checksum"], write_to_standard_in=list_of_data_obj_as_json)
 
     @staticmethod
-    def run_query(self, arguments: List[str], input_json: dict=None, write_to_standard_in: List[str]=None):
+    # TODO: What is the difference between input_data and write to standard in?
+    def run_query(arguments: List[str], input_data: dict=None, write_to_standard_in: List[str]=()):
         """
         TODO
         :param arguments:
-        :param input_json:
+        :param input_data:
         :param write_to_standard_in:
         :return:
         """
@@ -99,7 +100,7 @@ class Baton:
             for to_write in write_to_standard_in:
                 process.stdin.write(to_write)
 
-        out, error = process.communicate(input=input_json)  # TODO: timeout=
+        out, error = process.communicate(input=input_data)  # TODO: timeout=
 
         if error:
             raise IOError("iRODs error : " + str(error))
@@ -113,7 +114,7 @@ class Baton:
         :return:
         """
         directory, file_name = os.path.split(irods_file_path)
-        return {'data_object' : file_name, 'collection' : directory}
+        return {"data_object" : file_name, "collection" : directory}
 
     @staticmethod
     def _convert_to_baton_avus(list_of_avu_tuples: List[Tuple[str, str]]) -> Mapping[str, List[Mapping[str, str]]]:
@@ -125,4 +126,4 @@ class Baton:
         irods_avu_list = []
         for attribute, value in list_of_avu_tuples:
             irods_avu_list.append({ "attribute": attribute, "value": value, "o": "="})
-        return {'avus' : irods_avu_list}
+        return {"avus" : irods_avu_list}
