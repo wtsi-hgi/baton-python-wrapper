6c21a22892fd347befacf5eb4f3291aef6546442
Colin Nolan <colin.nolan@sanger.ac.uk>
2016-01-21 14:23:54 +0000

    Defined JSON serialiser for `DataObjectReplicaCollection`.

--- baton/json_serialisation.py
+++ baton/json_serialisation.py
@@ -1,4 +1,19 @@
+from json import JSONEncoder
+
+from baton.collections import DataObjectReplicaCollection
+from hgicommon.json import DefaultSupportedReturnType
 from hgicommon.json_conversion import MetadataJSONEncoder
 
 # `IrodsMetadata` JSON encoder can be the same as the `Metadata` JSON encoder as it has no additional properties
 IrodsMetadataJSONEncoder = MetadataJSONEncoder
+
+
+class DataObjectReplicaCollectionJSONEncoder(JSONEncoder):
+    """
+    JSON encoder for `DataObjectReplicaCollection`.
+    """
+    def default(self, to_encode: DataObjectReplicaCollection) -> DefaultSupportedReturnType:
+        if not isinstance(to_encode, DataObjectReplicaCollection):
+            super().default(to_encode)
+
+        return to_encode._data
