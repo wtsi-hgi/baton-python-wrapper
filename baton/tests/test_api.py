6647e56fd7f031bae7d2301eda69a73e47ce86a1
Colin Nolan <colin.nolan@sanger.ac.uk>
2016-03-15 16:34:56 +0000

    Added tests for `connect_to_irods_with_baton`.

--- baton/tests/test_api.py
+++ baton/tests/test_api.py
@@ -1,6 +1,7 @@
 import unittest
+from unittest.mock import patch, MagicMock
 
-from baton.api import Connection
+from baton.api import Connection, connect_to_irods_with_baton
 from baton._baton_mappers import BatonCollectionMapper, BatonDataObjectMapper, BatonSpecificQueryMapper
 from baton.tests._settings import BATON_DOCKER_BUILD
 from testwithbaton.api import TestWithBatonSetup
@@ -26,6 +27,22 @@ class TestConnection(unittest.TestCase):
 
     def tearDown(self):
         self.test_with_baton.tear_down()
+
+
+class TestConnectToIrodsWithBaton(unittest.TestCase):
+    """
+    Tests for `connect_to_irods_with_baton` method.
+    """
+    @patch("baton.api.Connection.__init__", MagicMock(return_value=None))
+    def test_argument_passing(self):
+        baton_binaries_directory = "location"
+        skip_baton_binaries_validation = True
+        connection = connect_to_irods_with_baton(baton_binaries_directory, skip_baton_binaries_validation)
+        connection.__init__.assert_called_with(baton_binaries_directory, skip_baton_binaries_validation)
+
+    def test_connection_returned(self):
+        connection = connect_to_irods_with_baton("somewhere", skip_baton_binaries_validation=True)
+        self.assertIsInstance(connection, Connection)
 
 
 if __name__ == "__main__":
