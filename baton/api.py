9917475c940a5d251652e30135c322e06203b37e
Colin Nolan <colin.nolan@sanger.ac.uk>
2016-03-15 15:12:53 +0000

    20: Added change to API.

--- baton/api.py
+++ baton/api.py
@@ -5,14 +5,16 @@ class Connection:
     """
     TODO
     """
-    def __init__(self, baton_binaries_directory: str,):
+    def __init__(self, baton_binaries_directory: str, skip_baton_binaries_validation: bool=False):
         """
         Constructor.
         :param baton_binaries_directory: the directory host of the baton binaries
+        :param skip_baton_binaries_validation: whether checks on if the correct baton binaries exist within the given
+        directory should be skipped
         """
-        self.data_object = BatonDataObjectMapper(baton_binaries_directory)
-        self.collection = BatonCollectionMapper(baton_binaries_directory)
-        self.specific_query = BatonSpecificQueryMapper(baton_binaries_directory)
+        self.data_object = BatonDataObjectMapper(baton_binaries_directory, skip_baton_binaries_validation)
+        self.collection = BatonCollectionMapper(baton_binaries_directory, skip_baton_binaries_validation)
+        self.specific_query = BatonSpecificQueryMapper(baton_binaries_directory, skip_baton_binaries_validation)
 
 
 def connect_to_irods_with_baton(baton_binaries_directory: str) -> Connection:
