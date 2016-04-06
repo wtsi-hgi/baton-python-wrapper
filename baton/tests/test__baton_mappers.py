4e5e357bb576be5569a499fd567504068ec97a69
Colin Nolan <colin.nolan@sanger.ac.uk>
2016-04-06 10:54:16 +0100

    Added tests for #18.

--- baton/tests/test__baton_mappers.py
+++ baton/tests/test__baton_mappers.py
@@ -5,7 +5,7 @@ from typing import Sequence
 from unittest.mock import MagicMock
 
 from baton._baton_mappers import BatonDataObjectMapper, BatonCollectionMapper, _BatonIrodsEntityMapper, \
-    BatonSpecificQueryMapper
+    BatonSpecificQueryMapper, BatonIrodsMetadataMapper
 from baton.collections import IrodsMetadata
 from baton.models import IrodsEntity, DataObject, Collection, PreparedSpecificQuery, SpecificQuery, SearchCriterion
 from baton.tests._helpers import combine_metadata, create_data_object, create_collection, synchronise_timestamps
@@ -19,6 +19,79 @@ _NAMES = ["name_1", "name_2", "name_3"]
 _ATTRIBUTES = ["attribute_1", "attribute_2"]
 _VALUES = ["value_1", "value_2", "value_3"]
 _UNUSED_VALUE = "value_4"
+
+
+class TestBatonIrodsMetadataMapper(unittest.TestCase):
+    """
+    Tests for `BatonIrodsMetadataMapper`.
+    """
+    def setUp(self):
+        self.test_with_baton = TestWithBatonSetup(baton_docker_build=BATON_DOCKER_BUILD)
+        self.test_with_baton.setup()
+        self.setup_helper = SetupHelper(self.test_with_baton.icommands_location)
+        self.mapper = BatonIrodsMetadataMapper(self.test_with_baton.baton_location)
+        self.metadata = IrodsMetadata({"key_1": {"value_1", "value_2"}, "key_2": {"value_3"}})
+
+    def test_get_all_when_invalid_path(self):
+        self.assertRaises(ValueError, self.mapper.get_all, "/invalid")
+
+    def test_get_all(self):
+        entity = create_data_object(self.test_with_baton, _NAMES[0], self.metadata)
+        self.assertEqual(self.mapper.get_all(entity.path), self.metadata)
+
+    def test_add_when_invalid_path(self):
+        self.assertRaises(ValueError, self.mapper.add, "/invalid", self.metadata)
+
+    def test_add(self):
+        entity = create_data_object(self.test_with_baton, _NAMES[0], IrodsMetadata())
+        self.mapper.add(entity.path, self.metadata)
+        self.assertEqual(self.mapper.get_all(entity.path), self.metadata)
+
+    def test_add_metadata_with_same_key(self):
+        entity = create_data_object(self.test_with_baton, _NAMES[0], self.metadata)
+        del self.metadata["key_1"]
+        self.assertRaises(ValueError, self.mapper.add, entity.path, self.metadata)
+
+    def test_set_when_invalid_path(self):
+        self.assertRaises(ValueError, self.mapper.set, "/invalid", self.metadata)
+
+    def test_set_when_no_existing_metadata(self):
+        entity = create_data_object(self.test_with_baton, _NAMES[0], IrodsMetadata())
+        self.mapper.set(entity.path, self.metadata)
+        self.assertEqual(self.mapper.get_all(entity.path), self.metadata)
+
+    def test_set_when_existing_non_duplicate_metadata(self):
+        entity = create_data_object(self.test_with_baton, _NAMES[0], IrodsMetadata({"another": {"value"}}))
+        self.mapper.set(entity.path, self.metadata)
+        self.assertEqual(self.mapper.get_all(entity.path), self.metadata)
+
+    def test_set_when_existing_duplicate_metadata(self):
+        entity = create_data_object(self.test_with_baton, _NAMES[0], self.metadata)
+        self.mapper.set(entity.path, self.metadata)
+        self.assertEqual(self.mapper.get_all(entity.path), self.metadata)
+
+    def test_remove_when_invalid_path(self):
+        self.assertRaises(ValueError, self.mapper.remove, "/invalid", self.metadata)
+
+    def test_remove_unset_metadata(self):
+        entity = create_data_object(self.test_with_baton, _NAMES[0], IrodsMetadata())
+        self.assertRaises(KeyError, self.mapper.remove, entity.path, self.metadata)
+
+    def test_remove_partially_unset_metadata(self):
+        partial_metadata = deepcopy(self.metadata)
+        del partial_metadata["key_1"]
+        entity = create_data_object(self.test_with_baton, _NAMES[0], partial_metadata)
+        self.assertRaises(KeyError, self.mapper.remove, entity.path, self.metadata)
+
+    def test_remove(self):
+        entity = create_data_object(self.test_with_baton, _NAMES[0], self.metadata)
+        assert len(self.metadata) == 2
+        partial_metadata_1 = deepcopy(self.metadata)
+        del partial_metadata_1["key_1"]
+        partial_metadata_2 = deepcopy(self.metadata)
+        del partial_metadata_2["key_2"]
+        self.mapper.remove(entity.path, partial_metadata_1)
+        self.assertEqual(self.mapper.get_all(entity.path), partial_metadata_2)
 
 
 class _TestBatonIrodsEntityMapper(unittest.TestCase, metaclass=ABCMeta):
@@ -317,7 +390,7 @@ class TestBatonInstalledSpecificQueryMapper(unittest.TestCase):
         return specific_queries
 
 
-# Trick required to stop Python's unittest from running the abstract base class as a test
+# Trick required to stop Python's unittest from running the abstract base classes as tests
 del _TestBatonIrodsEntityMapper
 
 
